263857f7604e78f0727bea5acc942c327f6e860b
Optimus115 <91112019+Optimus115@users.noreply.github.com>
2024-05-29 01:14:09 +0530

    add Vertical Navigation  and Horizental Also

---
+++ src/Components/LoginButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './css.files/login.css';
+function LoginButton(){
+    return(
+        <div className="login-button-container">
+        <button className="login-button">Login</button>
+    </div>
+    )
+}
+export default LoginButton;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './css.files/Navbar.css';
+function Navigation(){
+    return (
+        <div className="vertical-nav bg-dark">
+            <div className="login-button-container">
+                <button className="login-button">Login</button>
+            </div>
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                    <Link className='nav-link'to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                <Link className='nav-link'to="/">Services</Link>
+                </li>
+                <li className="nav-item">
+                <Link className='nav-link'to="/">About</Link>
+                </li>
+                <li className="nav-item">
+                <Link className='nav-link'to="/">Contact</Link>
+                </li>
+            </ul>
+
+        </div>
+    
+    );
+}
+export default Navigation;
